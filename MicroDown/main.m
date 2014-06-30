3f41f5cdd62f1d895aee9daff8623ca1a4459296
Jingkai He <jaxihe@gmail.com>
2014-06-30 11:58:51 +0100

    Initial Commit

---
+++ MicroDown/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MicroDown
+//
+//  Created by Jingkai He on 30/06/2014.
+//  Copyright (c) 2014 Jingkai He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

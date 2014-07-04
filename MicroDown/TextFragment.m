df9e0f54a5e977bc6a7b3504a9af8c27dd964734
Jingkai He <jaxihe@gmail.com>
2014-07-04 10:26:41 +0100

    image regexes...

--- MicroDown/TextFragment.m
+++ MicroDown/TextFragment.m
@@ -11,8 +11,18 @@
 static NSString *emailPattern = @"([A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4})";
 static NSString *emailFormat = @"<a href=\"mailto:%@\" target=\"_top\">Send Mail</a>";
 
-static NSString *linkPattern = @"((http|https|ftp)://((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+)";
-static NSString *linkFormat = @"<a href=\"%@\" />";
+static NSString *autolinkPattern = @"((http|https|ftp)://((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+)";
+static NSString *autolinkFormat = @"<a href=\"%@\" />";
+
+
+static NSString *linkPattern;
+static NSString *linkFormat;
+
+static NSString *imageInlinePattern = @"\\!\\[(.+?)\\]\\s*\\((.+?)\\)";
+static NSString *imageInlineFormat = @"<image src=\"%@\" alt=\"%@\" />";
+
+static NSString *imageReferencePattern = @"\\!\\[(.+?)\\]\\s*\\[(.+?)\\]";
+static NSString *imageReferenceFormat = @"<image src=\"%@\" alt=\"%@\" />";
 
 static NSString *boldPattern = @"\\*([^\n\\ ][^\n]+?[^\n\\ ])\\*";
 static NSString *boldFormat = @"<strong>%@</strong>";
@@ -32,14 +42,37 @@ static NSString *delFormat = @"<del>%@</del>";
 
 - (NSString *) toHTML
 {
-    [self del];
+    [self plainConvert];
     
     return self.content;
 }
 
-- (void) del
+- (void) plainConvert
 {
-    self.content = [self replaceContextWithPattern:delPattern withFormat:delFormat];
+    [self convertInlineImage];
+    [self convertReferenceImage];
+//    self.content = [self replaceContextWithPattern:delPattern withFormat:delFormat];
+//    self.content = [self replaceContextWithPattern:emailPattern withFormat:emailFormat];
+//    self.content = [self replaceContextWithPattern:autolinkPattern withFormat:autolinkFormat];
+//    self.content = [self replaceContextWithPattern:boldPattern withFormat:boldFormat];
+}
+
+- (void) convertInlineImage
+{
+    NSError *error = error;
+    
+    NSRegularExpression * regex = [NSRegularExpression regularExpressionWithPattern:imageInlinePattern options:0 error:&error];
+    
+    self.content = [regex stringByReplacingMatchesInString:self.content options:0 range:NSMakeRange(0, [self.content length]) withTemplate:[NSString stringWithFormat:imageInlineFormat, @"$2", @"$1"]];
+}
+
+- (void) convertReferenceImage
+{
+    NSError *error = error;
+    
+    NSRegularExpression * regex = [NSRegularExpression regularExpressionWithPattern:imageReferencePattern options:0 error:&error];
+    
+    self.content = [regex stringByReplacingMatchesInString:self.content options:0 range:NSMakeRange(0, [self.content length]) withTemplate:[NSString stringWithFormat:imageReferenceFormat, @"$2", @"$1"]];
 }
 
 @end
--- MicroDown/main.m
+++ MicroDown/main.m
@@ -20,6 +20,9 @@ int main(int argc, const char * argv[])
         HeadingFragment *heading = [[HeadingFragment alloc] initWithContent:@"## bacdefg"];
         NSLog(@"%@", [heading toHTML]);
         
+        TextFragment *text = [[TextFragment alloc] initWithContent:@"![alt text]   [https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png]"];
+        NSLog(@"%@", [text toHTML]);
+        
         NSLog(@"==========================");
         
         Document *doc = [[Document alloc] initWithContent:@"line1\nline2\n\nnew paragraph\n    some space\nsame paragraph\n\nnew p"];

3030003a55adb3864fadcc914546ff2e40ec6b51
Jingkai He <jaxihe@gmail.com>
2014-08-10 15:17:36 +0100

    benchmark changed

--- MicroDown/main.m
+++ MicroDown/main.m
@@ -11,12 +11,17 @@
 
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
         
-        NSDate *start = [NSDate date];
+        NSMutableString *content = [[NSMutableString alloc] init];
         for (int i = 0; i < 10000; ++i) {
-            Document *doc = [[Document alloc] initWithContent:@"## Hello world\nHello world\n---\nHello World\n\nefgh\n\n1. *abcde*\n- b\n* c\n\n---\n<jaxihe@gmail.com><https://google.com>![http://image.com][your image]!v[https://www.youtube.com/embed/slNHuM4X-8M][500x400]"];
+            [content appendString:@"## Hello world\nHello world\n---\nHello World\n\nefgh\n\n1. *abcde*\n- b\n* c\n\n---\n<jaxihe@gmail.com><https://google.com>![http://image.com][your image]!v[https://www.youtube.com/embed/slNHuM4X-8M][500x400]"];
+        }
+        
+        NSDate *start = [NSDate date];
+        for (int i = 0; i < 10; ++i) {
+            Document *doc = [[Document alloc] initWithContent:content];
+            
             Parser *parser = [[Parser alloc] initWithDocument:doc];
             [parser parse];
             [parser render];
